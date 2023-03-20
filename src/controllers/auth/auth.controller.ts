38e4ba5d4f02fc8d2c98c81b9b8677f07b27edea
machiavelio <machiaveliogb@gmail.com>
2023-03-21 00:44:51 +0200

    some auth

--- src/controllers/auth/auth.controller.ts
+++ src/controllers/auth/auth.controller.ts
@@ -1,19 +1,33 @@
-import { Controller, Get } from "@nestjs/common";
+import { Body, Controller, Get, HttpException, HttpStatus, Post, Req } from "@nestjs/common";
+import { Query } from "@nestjs/common/decorators";
+import { CreateUserModel } from "./models/create-user.model";
 import { AuthService } from "./services/auth.service";
 
 @Controller("auth")
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
-  @Get()
-  async login() {
-    return await this.authService.findByUsername("a");
-    return true;
+  @Get("check")
+  async check(@Query() { username }: { username: string }) {
+    const response = await this.authService.existsUserWithUsername(username);
+
+    return response;
   }
 
-  @Get("create")
-  register() {
-    this.authService.createUser();
-    return true;
+  @Post("connect")
+  async login(@Body() user: CreateUserModel) {
+    const foundUser = await this.authService.getUser(user);
+
+    if (!foundUser) {
+      throw new HttpException("Invalid credentials", HttpStatus.UNAUTHORIZED);
+    }
+    return foundUser;
+  }
+
+  @Post("create")
+  async register(@Body() user: CreateUserModel) {
+    const createdUser = await this.authService.createUser(user);
+
+    return createdUser;
   }
 }
--- src/controllers/auth/services/auth.service.ts
+++ src/controllers/auth/services/auth.service.ts
@@ -1,19 +1,40 @@
 import { Injectable } from "@nestjs/common";
 import { InjectModel } from "@nestjs/mongoose";
 import { Model } from "mongoose";
+import { CreateUserModel } from "../models/create-user.model";
+import { ConnectUserModel } from "../models/connect-user.model";
 import { User, UserDocument } from "../schemas/user.schema";
+import { UserModel } from "../models/user.model";
 
 @Injectable()
 export class AuthService {
   constructor(@InjectModel(User.name) private readonly userModel: Model<UserDocument>) {}
 
-  async findByUsername(username: string) {
-    const response = await this.userModel.find({ username: username });
+  async existsUserWithUsername(username: string) {
+    const user = await this.userModel.findOne({ username: username });
 
-    return response;
+    return !!user;
   }
 
-  createUser() {
-    this.userModel.create({ username: "a", password: "1234" });
+  async getUser(user: ConnectUserModel) {
+    const foundUser = await this.userModel.findOne({ username: user.username });
+
+    if (foundUser.password === user.password) {
+      return {
+        id: foundUser.id,
+        username: foundUser.username,
+      } as UserModel;
+    }
+
+    return null;
+  }
+
+  async createUser(user: CreateUserModel) {
+    const createdUser = await this.userModel.create({ username: user.username, password: user.password });
+
+    return {
+      id: createdUser.id,
+      username: createdUser.username,
+    } as UserModel;
   }
 }
